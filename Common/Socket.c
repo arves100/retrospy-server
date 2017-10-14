99af4d4268194748af835097221be18df16a7be2
Arves100 <arves100@gmail.com>
2017-10-14 22:46:42 +0200

    Added connect, multi socket to Socket.c

--- Common/Socket.c
+++ Common/Socket.c
@@ -1,24 +1,74 @@
-#include "Common.h"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
 
-#ifdef PLATFORM_WINDOWS
+#define GAMESPY_BUFLEN 1024
 
-SOCKET socket_listensocket = INVALID_SOCKET, socket_clientsocket = INVALID_SOCKET;
+#include "Socket.h"
 
-int socket_init(int port)
+int socket_init()
 {
-	WSADATA wsaData;
 	int iResult = 0;
-	struct addrinfo *result = NULL, *ptr = NULL, hints;
-	char real_port[6] = { 0 };
-
-	sprintf_s(real_port, 6, "%d", port);
-
+	WSADATA wsaData;
+	
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // Initalize WS2
 	if (iResult != 0)
 	{
 		printf("Error in socket_init: WSAStartup returned %d\n", iResult);
 		return 0;
 	}
+	
+	return 1;
+}
+
+int socket_connect(const char *ip, int port, SOCKET* dest)
+{
+	char sport[6] = { 0 };
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	int ir = 0;
+	
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	
+	sprintf_s(sport, 6, "%d", port);
+
+	ir = getaddrinfo(ip, sport, &hints, &result);
+
+	if (ir != 0) {
+		return 0;
+	}
+
+	ptr = result;
+
+	*dest = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+	if (*dest == INVALID_SOCKET) {
+		return 0;
+	}
+
+	ir = connect(*dest, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (ir == SOCKET_ERROR || *dest == INVALID_SOCKET) {
+		closesocket(*dest);
+		freeaddrinfo(result);
+		return 0;
+	}
+
+	freeaddrinfo(result);
+
+	return 1;
+}
+
+int socket_bind(int port, SOCKET* dest)
+{
+	int iResult = 0;
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	char real_port[6] = { 0 };
+
+	sprintf_s(real_port, 6, "%d", port);
 
 	// Setup listensocket structure
 	ZeroMemory(&hints, sizeof(hints));
@@ -31,40 +81,37 @@ int socket_init(int port)
 	iResult = getaddrinfo(NULL, real_port, &hints, &result);
 	if (iResult != 0)
 	{
-		printf("Error in socket_init: getaddrinfo returned %d\n", iResult);
+		printf("Error in socket_bind: getaddrinfo returned %d\n", iResult);
 		WSACleanup();
 		return 0;
 	}
 
 	// Create the listen socket
-	socket_listensocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (socket_listensocket == INVALID_SOCKET)
+	*dest = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (*dest == INVALID_SOCKET)
 	{
-		printf("Error in socket_init: socket returned %ld\n", WSAGetLastError());
+		printf("Error in socket_bind: socket returned %ld\n", WSAGetLastError());
 		freeaddrinfo(result);
-		WSACleanup();
 		return 0;
 	}
 
 	// Bind the socket
-	iResult = bind(socket_listensocket, result->ai_addr, (int)result->ai_addrlen);
+	iResult = bind(*dest, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR)
 	{
-		printf("Error in socket_init: bind returned %d\n", iResult);
+		printf("Error in socket_bind: bind returned %d\n", iResult);
 		freeaddrinfo(result);
-		closesocket(socket_listensocket);
-		WSACleanup();
+		closesocket(*dest);
 		return 0;
 	}
 
 	freeaddrinfo(result);
 
 	// Start listening on the socket
-	if (listen(socket_listensocket, SOMAXCONN) == SOCKET_ERROR)
+	if (listen(*dest, SOMAXCONN) == SOCKET_ERROR)
 	{
-		printf("Error in socket_init: listen returned %ld\n", WSAGetLastError());
-		closesocket(socket_listensocket);
-		WSACleanup();
+		printf("Error in socket_bind: listen returned %ld\n", WSAGetLastError());
+		closesocket(*dest);
 		return 0;
 	}
 
@@ -73,15 +120,13 @@ int socket_init(int port)
 
 void socket_destroy()
 {
-	closesocket(socket_clientsocket);
-	closesocket(socket_listensocket); // Close the listensocket
 	WSACleanup(); // Destroy WS2
 }
 
-int socket_accept()
+int socket_accept(SOCKET listen, SOCKET *dest)
 {
-	socket_clientsocket = accept(socket_listensocket, NULL, NULL);
-	if (socket_clientsocket == INVALID_SOCKET)
+	*dest = accept(listen, NULL, NULL);
+	if (*dest == INVALID_SOCKET)
 	{
 		printf("Error in socket_accept: accept returned %ld\n", WSAGetLastError());
 		return 0;
@@ -90,12 +135,12 @@ int socket_accept()
 	return 1;
 }
 
-char* socket_recv(int *lenout)
+char* socket_recv(SOCKET sock, int *lenout)
 {
 	char recvbuf[GAMESPY_BUFLEN] = { 0 };
 	int recvbuflen = GAMESPY_BUFLEN;
 
-	*lenout = recv(socket_clientsocket, recvbuf, recvbuflen, 0);
+	*lenout = recv(sock, recvbuf, recvbuflen, 0);
 	if (*lenout > 0)
 	{
 		printf("socket_recv: received %d bytes\n", *lenout);
@@ -110,17 +155,16 @@ char* socket_recv(int *lenout)
 	return NULL;
 }
 
-int socket_send(char *input, int length)
+int socket_send(SOCKET sock, char *input, int length)
 {
 	int iResult;
-	iResult = send(socket_clientsocket, input, length, 0);
+	iResult = send(sock, input, length, 0);
 	if (iResult == SOCKET_ERROR)
 	{
 		printf("Error in socket_send: send error: %d\n", WSAGetLastError());
 		return 1;
 	}
 
-	printf("socket_recv: sended %d bytes\n", length);
+	printf("socket_send: sended %d bytes\n", length);
 	return 0;
 }
-#endif
--- Common/Socket.h
+++ Common/Socket.h
@@ -1,10 +1,18 @@
 #ifndef _RSS_COMMON_SOCKET_H_
 #define _RSS_COMMON_SOCKET_H_
 
-extern C_LINKAGE int socket_init(int port);
-extern C_LINKAGE void socket_destroy();
-extern C_LINKAGE char *socket_recv(int *lenout);
-extern C_LINKAGE int socket_accept();
-extern C_LINKAGE int socket_send(char *input, int length);
+#ifdef __cplusplus
+	#define C_LINKAGE "C"
+#else
+	#define C_LINKAGE 
+#endif
+
+extern C_LINKAGE int socket_init(void);
+extern C_LINKAGE int socket_bind(int port, SOCKET* dest);
+extern C_LINKAGE void socket_destroy(void);
+extern C_LINKAGE char *socket_recv(SOCKET sock, int *lenout);
+extern C_LINKAGE int socket_accept(SOCKET listen, SOCKET* dest);
+extern C_LINKAGE int socket_send(SOCKET sock, char *input, int length);
+extern C_LINKAGE int socket_connect(const char *ip, int port, SOCKET* dest);
 
 #endif
